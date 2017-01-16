b79d5f96d0a73c3a9b6e3a14c8160cc0fa052642
Lea Douchet <lea.douchet@insa-lyon.fr>
2017-01-16 16:40:09 +0100

    ctring constructor

--- string.cpp
+++ string.cpp
@@ -1,10 +1,26 @@
 #include "string.h"
 #include <cstdlib>
-#include <stdio.h>
+#include <cstdio>
 
-//constructeur par defaut
+ 
+//constructors
 string::string(){
 	capacity_=10;
 	chaine_= new char[capacity_];
 	len_=0;
+}
+//
+string::string(const char* mot){
+	int i = 0;
+	capacity_ = 0;
+	while (mot[i] != '\0'){
+		++i;
+		++capacity_;
+	}
+	len_= capacity_;
+	chaine_ = new char[capacity_ + 1];
+	for (int k = 0; k <= capacity_ ; ++k){
+		chaine_[k]=mot[k];
+	}
+
 }
