971470b0a542d2d653f9c63a41ae83929cb1114f
Florian Peron <florian.peron@insa-lyon.fr>
2017-01-23 16:30:40 +0100

    test

--- test.cpp
+++ test.cpp
@@ -6,20 +6,11 @@ using std::endl;
 using std::cin;
 int main(){
 	string a = string();
-	char* phrase = new char[255];
-	cin >> phrase;
-	string b = string(phrase);
-	cout<< b.length() <<endl;
-	cout<<b.max_size()<<endl;
-	b.resize(7);
-	cout<<b.length()<<endl;
-	cout<<b.capacity()<<endl;
-	
-	string c = string(b);
-	cout << c.size() << endl;
+	char* mot1 = new char[255];
+	cin >> mot1;
+	string b = string(mot1);
 	a = 'c';
-	cout<<a.length()<<endl;
-	cout<<a.capacity()<<endl;
 	cout << a.c_str() << endl;
-
+	a = b;
+	cout << a.c_str() << endl;
 }
